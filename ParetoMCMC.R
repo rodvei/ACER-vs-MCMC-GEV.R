c2c9c377d76450cf59293e50662d6893af8f2cc0
unknown <rodvei@hotmail.com>
2015-12-01 18:55:49 +0100

    1/12 - 2015 6:55, everything is awesome

--- ParetoMCMC.R
+++ ParetoMCMC.R
@@ -7,10 +7,24 @@ lamda<-2.38^2/length(normMu)
 theta<-matrix(rep(0,2*n),2)
 for(i in 1:n){
   Xtemp<-mvrnorm(n=1,normMu,lamda*coVar)
-  
+  if(u<RGDP(data,Xtemp,theta[,(i-1)])){
+    theta[,i]<-Xtemp
+  }else{
+    theta[,i]<-theta[,(i-1)]
+  }
+  normMu<-XXX
+  covar<-XXX
   
 }
 
-RGDP<-function(Xstar,X){
-  
+
+lnGPD->function(data,si,xi){
+  return(-length(data)*log(si)-(1+1/xi)*sum(log(1+xi*data/si)))
+}
+
+RGDP<-function(data,Xtemp,X){
+lnGPNtemp<--length(data)*Xtemp[2]-(1+1/Xtemp[1])*sum(log(1+Xtemp[1]*data/exp(Xtemp[2])))
+lnGPN<--length(data)*X-(1+1/X[1])*sum(log(1+X[1]*data/exp(X[2])))
+
+return(exp( ))  
 }
